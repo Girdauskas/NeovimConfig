2be49d96c90de28ec134006aeb7726a85c3b4454
Matas Girdauskas <matas.girdauskas@gmail.com>
2025-07-08 22:00:06 +0300

    Enabling line numbers.

--- init.lua
+++ init.lua
@@ -1,10 +1,13 @@
 require("config.lazy")
 
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>fv", vim.cmd.Ex)
 
 -- Tab is 4 spaces!
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
+
+-- Line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
 
